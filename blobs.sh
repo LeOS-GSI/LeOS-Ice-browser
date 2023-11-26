c06f7f63de4ce3199a25fc8e03f415d0778cbee5
harvey186 <harvey186@hotmail.com>
2023-11-26 15:17:39 +0100

    x

--- blobs.sh
+++
@@ -1,6 +0,0 @@
-sed -i \
-    -e '/Deps.mozilla_lib_push_firebase/d' \
-    -e '/Deps.adjust/d; /Deps.installreferrer/d; /Deps.google_ads_id/d' \
-    -e '/Deps.google_play_store/d' \
-    app/build.gradle
-
